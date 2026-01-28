12f7802fc2bb9c95a43f4adc3a3cf5c18b12386f
Gabo <gabriel.herbei@outlook.com>
2026-01-28 09:23:02 +0100

    Updated version

--- qr-inventory/backend/qr-inventory/src/main/java/com/herbei/qr_inventory/repository/ItemRepository.java
+++ qr-inventory/backend/qr-inventory/src/main/java/com/herbei/qr_inventory/repository/ItemRepository.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long>
 {
-    Optional<Item> findNewestItem;
-    Optional<Item> findOldestItem;
+    //Optional<Item> findNewestItem;
+    //Optional<Item> findOldestItem;
 }
